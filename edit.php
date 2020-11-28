f74fbf2886584ff0ae3e89fa7a9565a3d1b799f5
Erin Urquhart <urquhart.erin@gmail.com>
2020-11-27 20:41:49 -0600

    CSS adjustment
    Began implementation for Bootstrap 4. Added the links and scripts required to every page and finished adjusting index.php to account for the new css.

--- edit.php
+++ edit.php
@@ -37,15 +37,18 @@
 ?> 
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Phoenix Books - Edit Book</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
-</head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </head>
 <body>
-    <div id="wrapper">
-      <div class="content">
+    <div class="container">
     <!-- notification message -->
     <?php if (isset($_SESSION['success'])) : ?>
       <div class="error success" >
@@ -57,8 +60,6 @@
         </h3>
       </div>
     <?php endif ?>
-
-      <div class="profile_info">
       <div>
         <?php  if (isset($_SESSION['user'])) : ?>
           <strong><?php echo $_SESSION['user']['username']; ?></strong>
@@ -119,6 +120,5 @@
         <div id="footer">
             PhoenixBooks 2020 - No Rights Reserved
         </div> <!-- END div id="footer" -->
-    </div> <!-- END div id="wrapper" -->
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!--Description: Main page of the site. Shows the last 5 books from the database. -->
+<!--Description: Main page of the site. Shows the books from the database. -->
 <?php
   
   require 'connect.php';
@@ -49,32 +49,31 @@
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Phoenix Books</title>
-    <link rel="stylesheet" href="style.css" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
-  <div id="wrapper">
-    <div class="content">
+    <div class="container">
     <!-- notification message -->
     <?php if (isset($_SESSION['success'])) : ?>
-      <div class="error success" >
-        <h3>
-          <?php 
-            echo $_SESSION['success']; 
-            unset($_SESSION['success']);
-          ?>
-        </h3>
+      <div class="alert alert-success alert-dismissible fade show" >
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+        <strong>Success!</strong> <?php echo $_SESSION['success'];  ?>
+        <?php  unset($_SESSION['success']); ?>
       </div>
     <?php endif ?>
 
-      <div class="profile_info">
+      <div class="container">
       <div>
         <?php  if (isset($_SESSION['user'])) : ?>
           <strong><?php echo $_SESSION['user']['username']; ?></strong>
-
           <small>
             <i  style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i> 
             <br>
@@ -83,7 +82,7 @@
 
         <?php endif ?>
       </div>
-      <div id="header">
+      <div class="jumbotron text-center" style="margin-bottom:0">
           <h1><a href="index.php">Phoenix Books</a></h1>
           <h2>Browse the books we have!</h2>
           <?php if (!isset($_SESSION['user'])) : ?>
@@ -92,19 +91,19 @@
           <?php endif ?>
       </div> <!-- END div id="header" -->
       <?php if (isset($_SESSION['user'])) :?>
-        <p>Sort by:</p>
         <form action="index.php" method="post">
-        <select name="orderby" id="orderby">
-          <option value="title" <?php echo (isset($_POST['orderby']) && $_POST['orderby'] == 'title') ? 'selected="selected"' : ''; ?>>Title</option>
-          <option value="author" <?php echo (isset($_POST['orderby']) && $_POST['orderby'] == 'author') ? 'selected="selected"' : ''; ?>>Author</option>
-          <option value="price" <?php echo (isset($_POST['orderby']) && $_POST['orderby'] == 'price') ? 'selected="selected"' : ''; ?>>Price</option>
-        </select>
-        <select name="order" id="order">
-          <option value="ASC" <?php echo (isset($_POST['order']) && $_POST['order'] == 'ASC') ? 'selected="selected"' : ''; ?>>Ascending</option>
-          <option value="DESC" <?php echo (isset($_POST['order']) && $_POST['order'] == 'DESC') ? 'selected="selected"' : ''; ?>>Descending</option>
-        </select>
-        <div id="categorysearch">
-            <label for="category">Category:</label>
+        <div class="form-group">
+          <select name="orderby" id="orderby">
+            <option value="title" <?php echo (isset($_POST['orderby']) && $_POST['orderby'] == 'title') ? 'selected="selected"' : ''; ?>>Title</option>
+            <option value="author" <?php echo (isset($_POST['orderby']) && $_POST['orderby'] == 'author') ? 'selected="selected"' : ''; ?>>Author</option>
+            <option value="price" <?php echo (isset($_POST['orderby']) && $_POST['orderby'] == 'price') ? 'selected="selected"' : ''; ?>>Price</option>
+          </select>
+          <select name="order" id="order">
+            <option value="ASC" <?php echo (isset($_POST['order']) && $_POST['order'] == 'ASC') ? 'selected="selected"' : ''; ?>>Ascending</option>
+            <option value="DESC" <?php echo (isset($_POST['order']) && $_POST['order'] == 'DESC') ? 'selected="selected"' : ''; ?>>Descending</option>
+          </select>
+        </div>
+        <div class="form-group">
             <select name="category" id="category">
               <option value="ALL">All</option>
             <?php while($row_category = $statement_category->fetch()): ?>
@@ -114,49 +113,51 @@
             <input type="submit" name="id" id="id" value="Search" />
             <input type="submit" name="command" value="Reset"/>
         </div>  
-        </form> 
+        </form>     
       <?php endif ?> 
-<ul id="menu">
-    <li><a href="index.php" class='active'>Home</a></li>
-    <?php if (isset($_SESSION['user'])) :?>
-      <?php if ($_SESSION['user']['user_type'] == 'admin') :?>
-        <li><a href="create_book.php" >New Book</a></li>
-        <li><a href="create_category.php" >New Category</a></li>
-        <li><a href="moderate_users.php" >Create/Delete Users</a></li>
-      <?php endif ?>
-    <?php endif ?>
-</ul> <!-- END div id="menu" -->
-<div id="all_books">
-  <!--while loop to display the books -->
-  <?php while ($row_book = $statement_book->fetch()): ?>
-      <div class="book_post">
-        <h2><a href="show.php?id=<?=$row_book['id']?>"><?= $row_book['title'] ?></a></h2>
-      <p>
-        <small>
-          Price: $<?= $row_book['price']?>
-        </small>
-      </p>
-       <p>
-        <small>
-          Author: <?= $row_book['author']?>
-        </small>
-      </p>
-      <p>
-        <small>
-          Category: <?= $row_book['category']?>
-        </small>
-      </p>
-      </div>
-      <div class='book_content'>
-        <!--if to truncate posts over 200 characters -->
-        <?php if (strlen($row_book['description']) > 200): ?>
-          <?= mb_strimwidth($row_book['description'], 0, 200, " ...");?>       
-          <a href="show.php?id=<?= $row_book['id'] ?>">Read more</a>
-          <?php else : ?>
-            <?= $row_book['description'] ?>     
+<nav class="navbar navbar-expand-sm bg-light justify-content-center">          
+  <ul class="navbar-nav">
+      <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+      <?php if (isset($_SESSION['user'])) :?>
+        <?php if ($_SESSION['user']['user_type'] == 'admin') :?>
+          <li class="nav-item"><a class="nav-link" href="create_book.php" >New Book</a></li>
+          <li class="nav-item"><a class="nav-link" href="create_category.php" >New Category</a></li>
+          <li class="nav-item"><a class="nav-link" href="moderate_users.php" >Create/Delete Users</a></li>
         <?php endif ?>
-      </div>
-  <?php endwhile ?>
+      <?php endif ?>
+  </ul>
+</nav> 
+<div class="container p-2 my-2 bg-light text-grey">
+  <div class="row">
+    <!--while loop to display the books -->
+    <?php while ($row_book = $statement_book->fetch()): ?>
+        <div class="col-sm-3">
+          <h2><a href="show.php?id=<?=$row_book['id']?>"><?= $row_book['title'] ?></a></h2>
+        <p>
+          <small>
+            Price: $<?= $row_book['price']?>
+          </small>
+        </p>
+         <p>
+          <small>
+            Author: <?= $row_book['author']?>
+          </small>
+        </p>
+        <p>
+          <small>
+            Category: <?= $row_book['category']?>
+          </small>
+        </p>
+          <!--if to truncate posts over 200 characters -->
+          <?php if (strlen($row_book['description']) > 200): ?>
+            <?= mb_strimwidth($row_book['description'], 0, 200, " ...");?>       
+            <a href="show.php?id=<?= $row_book['id'] ?>">Read more</a>
+            <?php else : ?>
+              <?= $row_book['description'] ?>     
+          <?php endif ?>
+        </div>
+    <?php endwhile ?>
+  </div>
 </div>
       <div id="footer">
           PhoenixBooks 2020 - No Rights Reserved
